1b8b3c4f9028d752fba82e2c3e72e17f1f0b61e2
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-07-22 23:44:36 +0530

    29

---
+++ 29.py
@@ -0,0 +1,27 @@
+class Solution:
+    def divide(self, dividend, divisor):
+        MAX_INT = 2**31 - 1
+        MIN_INT = -2**31
+
+        if dividend == MIN_INT and divisor == -1:
+            return MAX_INT
+
+        negative = (dividend < 0) != (divisor < 0)
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        quotient = 0
+
+        while dividend >= divisor:
+            temp = divisor
+            multiple = 1
+
+            while dividend >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+
+            dividend -= temp
+            quotient += multiple
+
+        return -quotient if negative else quotient
